85cfedc2b010c310eb1ba5bc0d90425c27adfce6
frogtube <u.pradere@gmail.com>
2017-12-28 11:39:00 +0100

    Phpunit/phpunit dependency added and test function passed

---
+++ tests/Entity/TrickTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Entity;
+
+
+use PHPUnit\Framework\TestCase;
+
+class TrickTest extends TestCase
+{
+    public function testThatPhpunitWorks()
+    {
+        $this->assertTrue(false);
+    }
+
+}

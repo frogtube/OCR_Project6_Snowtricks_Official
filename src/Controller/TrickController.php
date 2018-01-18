f1af433ceecf8587f57111273c0a2bee83ac2f1b
frogtube <u.pradere@gmail.com>
2018-01-18 09:31:13 +0100

    ListTrick and showTrick created. ShowTrick only displays data of trick but nothing about other entities (comments, images, videos). Commit for review

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -3,20 +3,46 @@
 namespace App\Controller;
 
 
+use App\Entity\Comment;
+use App\Entity\Trick;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class TrickController extends Controller
 {
     public function listAction() {
-        return $this->render('trick/listTrick.html.twig', array(
 
+        $tricks = $this->getDoctrine()
+            ->getRepository(Trick::class)
+            ->findAll();
+
+        return $this->render('trick/listTrick.html.twig', array(
+            'tricks' => $tricks,
         ));
     }
 
-    public function showAction() {
-        return $this->render('trick/showTrick.html.twig', array(
+    public function showAction($slug) {
 
+        $trickName = str_replace('_', ' ', $slug);
+        $trick = $this->getDoctrine()
+            ->getRepository(Trick::class)
+            ->findOneBy(array(
+                'name' => $trickName,
+            ));
+
+        $comments = $this->getDoctrine()
+            ->getRepository(Comment::class)
+            ->findBy(array(
+                'trick' => $trick->getId()
+            ));
+
+        if(!$trick) {
+            throw $this->createNotFoundException('No trick found for id'.$trickName);
+        }
+
+        return $this->render('trick/showTrick.html.twig', array(
+            'trick' => $trick,
+            'comments' => $comments
         ));
     }
 

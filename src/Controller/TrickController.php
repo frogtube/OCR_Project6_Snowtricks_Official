315facb9dfd5352e240f6ac9665eae6237dfc1de
frogtube <u.pradere@gmail.com>
2018-01-20 16:46:14 +0100

    trickType Form created and fonctionnal for trick parameters. Needs to add parameters from linked entities and validation

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Entity\Trick;
+use App\Form\TrickType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class TrickController extends Controller
@@ -30,7 +32,7 @@ class TrickController extends Controller
             ->getTrickWithComments($slug);
 
         if(!$trick) {
-            throw $this->createNotFoundException('No trick found for id'.$trickName);
+            throw $this->createNotFoundException('No trick found for id'.$slug);
         }
 
         return $this->render('trick/showTrick.html.twig', array(
@@ -39,9 +41,39 @@ class TrickController extends Controller
 
     }
 
-    public function createAction() {
-        return $this->render('trick/newTrick.html.twig', array(
+    public function createAction(Request $request) {
 
+        $trick = new Trick();
+        $trick->setCreatedAt(new \DateTime('now'));
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        // var_dump($trick);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() ) {
+
+            $trick = $form->getData();
+
+            // Creating slug by replacing name spaces with a dash
+            $trick->setSlug(str_replace(' ', '-', $trick->getName()));
+            //ADD HERE THE $trick->setUser WITH THE COOKIE
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($trick);
+            $em->flush();
+
+            return $this->redirectToRoute('trick_show', [
+                'slug' => $trick->getSlug()
+            ]);
+        }
+
+
+
+        return $this->render('trick/newTrick.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 
